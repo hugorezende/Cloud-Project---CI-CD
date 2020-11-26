27fd05dc8d195aab0b5181fca309ed6848d36445
Hugo Rezende <hugorezendedev@gmail.com>
2020-11-26 14:29:59 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Modify name <code>src/App.js</code>
+          Hugo <code>src/App.js</code>
         </p>
 
         <a
